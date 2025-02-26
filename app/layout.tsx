7eddfa7203d4094fab3821542f780e484419e155
Divyanshu-Bhandari <divyanshub120130@gmail.com>
2025-02-26 18:23:08 +0530

    Added vercel speed_insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const font = Open_Sans({ subsets: ["latin"] });
 
